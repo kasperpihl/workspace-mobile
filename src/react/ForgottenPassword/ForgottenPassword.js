bae849852b71fbd0ff6386b5bf7a453d855bd513
thinklinux <tihomir.valkanov@gmail.com>
2019-01-15 10:23:38 +0200

    forgotten password flow

--- src/react/ForgottenPassword/ForgottenPassword.js
+++ src/react/ForgottenPassword/ForgottenPassword.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Alert } from 'react-native';
+import request from 'swipes-core-js/utils/request';
 import { goSignIn } from 'src/navigation';
 import Input from 'src/react/Input/Input';
 import FormButton from 'src/react/FormButton/FormButton';
 import SW from './ForgottenPassword.swiss';
 
-export default class SignIn extends Component {
+export default class ForgottenPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailVal: '',
+    };
+    this.handleSendResetEmail = this.handleSendResetEmail.bind(this);
+  }
+  handleChangeText = field => value => {
+    this.setState({ [field]: value });
+  };
+  handleSendResetEmail() {
+    const { emailVal } = this.state;
+
+    request('user.sendResetEmail', {
+      email: emailVal,
+    }).then(res => {
+      if (res.ok === false) {
+        Alert.alert('Invalid email', '', [{ text: 'OK' }], {
+          cancelable: false,
+        });
+      } else {
+        Alert.alert(
+          'Success',
+          'Check your email for instructions to reset your password',
+          [
+            {
+              text: 'OK',
+              onPress: () => {
+                goSignIn();
+              },
+            },
+          ],
+          {
+            cancelable: false,
+          }
+        );
+      }
+    });
+  }
   render() {
     return (
       <SW.Wrapper>
@@ -14,9 +55,16 @@ export default class SignIn extends Component {
           {`Enter you email and we'll send you an email to reset your password.`}
         </SW.CopyText>
         <SW.FormWrapper>
-          <Input label={'Email'} textContentType={'username'} />
+          <Input
+            label={'Email'}
+            textContentType={'username'}
+            onChangeText={this.handleChangeText('emailVal')}
+          />
           <View style={{ marginTop: 80 }}>
-            <FormButton label={'Reset password'} />
+            <FormButton
+              label={'Reset password'}
+              onPress={this.handleSendResetEmail}
+            />
             <Button
               onPress={() => {
                 goSignIn();
