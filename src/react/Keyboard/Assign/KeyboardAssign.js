ae56c573919c4cffc83a034f5dbea1527e0e7aa2
thinklinux <tihomir.valkanov@gmail.com>
2019-04-12 14:21:53 +0300

    fix assign to tasks and design

--- src/react/Keyboard/Assign/KeyboardAssign.js
+++ src/react/Keyboard/Assign/KeyboardAssign.js
@@ -1,10 +1,22 @@
 import React, { PureComponent } from 'react';
-import { VirtualizedList } from 'react-native';
+import { ScrollView } from 'react-native';
 import Promo from 'src/react/Promo/Promo';
 import KeyboardAssignUserItem from 'src/react/Keyboard/Assign/UserItem/KeyboardAssignUserItem';
+import Picker from 'src/react/Picker/Picker';
+import userGetFullName from 'core/utils/user/userGetFullName';
+import AssignItem from 'src/react/AssignItem/AssignItem';
+import { List } from 'immutable';
 import SW from './KeyboardAssign.swiss';
 
 export default class KeyboardAssign extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const assignees = props.lastSelectedTask.get('assignees');
+    this.state = {
+      selectedPeople: assignees.toJS() || [],
+    };
+  }
   getItemCount = data => data.size;
   getItem = (data, index) => ({
     key: data.getIn([index, 'user_id']),
@@ -24,43 +36,67 @@ export default class KeyboardAssign extends PureComponent {
       />
     );
   };
-  onItemPressed = (user_id, assigned) => {
-    const { stateManager, lastSelectedTask } = this.props;
-    const assignees = lastSelectedTask.get('assignees');
-    let finalAssignees;
-
-    if (assigned) {
-      finalAssignees = assignees.push(user_id);
-    } else {
-      finalAssignees = assignees.filter(x => x !== user_id);
-    }
-
+  handlePeopleChange = value => {
+    const { lastSelectedTask, stateManager } = this.props;
+    this.setState({
+      selectedPeople: value,
+    });
     stateManager.editHandler.updateAssignees(
       lastSelectedTask.get('task_id'),
-      finalAssignees
+      new List(value)
     );
   };
-  render() {
+  preparePeopleValuesForPicker = () => {
     const { users } = this.props;
+    const { selectedPeople } = this.state;
+
+    if (!users) return [];
+
+    const items = [];
+
+    users
+      .filter(user => user.get('status') !== 'disabled')
+      .forEach((user, key) => {
+        const userId = user.get('user_id');
+        const teamId = user.get('team_id');
+        const fullName = userGetFullName(userId, teamId);
+
+        items.push(
+          <AssignItem
+            size={30}
+            key={key}
+            userId={userId}
+            teamId={teamId}
+            fullName={fullName}
+            assigned={selectedPeople.includes(userId)}
+          />
+        );
+      });
+
+    return items;
+  };
+  render() {
+    const { users, lastSelectedTask } = this.props;
+    const { selectedPeople } = this.state;
 
     if (!users) {
       return <Promo />;
     }
 
-    const filteredUsers = users
-      .toList()
-      .filter(user => user.get('status') !== 'disabled');
+    const taskId = lastSelectedTask.get('task_id');
 
     return (
-      <SW.Wrapper>
-        <VirtualizedList
-          alwaysBounceVertical={false}
-          getItem={this.getItem}
-          getItemCount={this.getItemCount}
-          data={filteredUsers}
-          renderItem={this.renderItem}
-        />
-      </SW.Wrapper>
+      <ScrollView alwaysBounceVertical={false}>
+        <SW.Wrapper>
+          <Picker
+            key={taskId}
+            multiselect={true}
+            values={this.preparePeopleValuesForPicker()}
+            onChange={this.handlePeopleChange}
+            defaultValue={selectedPeople}
+          />
+        </SW.Wrapper>
+      </ScrollView>
     );
   }
 }
