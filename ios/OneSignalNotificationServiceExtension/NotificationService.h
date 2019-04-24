cbb29197e12f02b61d6a6d45b95b90f2e4da3829
thinklinux <tihomir.valkanov@gmail.com>
2019-04-24 12:19:46 +0300

    fixing push notifications on ios

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Tihomir Valkanov on 23.04.19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
--- src/react/HOCRootWrapper.js
+++ src/react/HOCRootWrapper.js
@@ -4,6 +4,7 @@ import { SwissProvider } from 'swiss-react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { SafeAreaView } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import CoreProvider from 'core/react/_hocs/CoreProvider';
 import Redirecter from 'src/Redirecter';
 import InitLoading from 'src/react/InitLoading/InitLoading.js';
@@ -11,6 +12,36 @@ import 'src/swiss/SwissInit';
 
 export default function RootWrapper(Component, store, persistor, requireAuth) {
   return class StoreWrapper extends React.Component {
+    constructor(props) {
+      super(props);
+      OneSignal.init('2e131867-f1c1-4af5-8188-23fcf8978a22');
+
+      OneSignal.addEventListener('received', this.onReceived);
+      OneSignal.addEventListener('opened', this.onOpened);
+      OneSignal.addEventListener('ids', this.onIds);
+    }
+
+    componentWillUnmount() {
+      OneSignal.removeEventListener('received', this.onReceived);
+      OneSignal.removeEventListener('opened', this.onOpened);
+      OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    onReceived(notification) {
+      console.log('Notification received: ', notification);
+    }
+
+    onOpened(openResult) {
+      console.log('Message: ', openResult.notification.payload.body);
+      console.log('Data: ', openResult.notification.payload.additionalData);
+      console.log('isActive: ', openResult.notification.isAppInFocus);
+      console.log('openResult: ', openResult);
+    }
+
+    onIds(device) {
+      console.log('Device info: ', device);
+    }
+
     render() {
       return (
         <Provider store={store}>
